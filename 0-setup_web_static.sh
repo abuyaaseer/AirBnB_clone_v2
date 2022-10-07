6b4e07c6fc4c0241bb7e98ddf3b8618a154bcd79
Ayomide Suara <aysuarex@gmail.com>
2022-10-06 20:18:41 -0700

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,28 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
-
+# Bash script that sets up the server for the deployment of web-static
+# Install Nginx server if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo service nginx start
+# Create folder /data/web_static/releases/test/ if not exists
 mkdir -p /data/web_static/releases/test/
+# Create folder /data/web_static/shared/ if not exists
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+# Create a fake HTML file /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Success</body></html>" > /data/web_static/releases/test/index.html
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+# if link exists, delete and remake each time the script is run
+if [ -h '/data/web_static/current' ]
+then
+	rm /data/web_static/current
+	ln -s /data/web_static/releases/test/ /data/web_static/current
+else
+	ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+# Give ownership of /data/ folder to ubuntu user and group, recursively
+chown -R ubuntu: /data/
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+TO_FIND="^server {$"
+TO_ADD="server {\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n"
+sudo sed -i "s/$TO_FIND/$TO_ADD/" /etc/nginx/sites-available/default
+sudo service nginx reload
